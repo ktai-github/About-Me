961b13d1de10908ac9e886dc9bb8542533afdd59
KevinT <kevin.tai.mail@gmail.com>
2017-10-16 19:55:01 -0700

    first commit - about-me

---
+++ About me/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  About me
+//
+//  Created by Kevin T on 2017-10-07.
+//  Copyright © 2017 Kevin T. All rights reserved.
+//
+
+//include standard i/o library
+#include <stdio.h>
+
+//the main function returns an integer type of value
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    //printing my name
+    printf("I am Kevin..\n");
+    
+    //printing the development platform that I like
+    printf("I like iOS\n");
+    
+    //printing what I want to buy
+    printf("Need to buy a new MacBook lol\n");
+    
+    //return integer value of 0
+    return 0;
+}
